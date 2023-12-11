805a918c25bae5791efc41fd661ec9ccd158e51a
Rebeca <rstingu@gannett.com>
2023-12-11 20:49:17 +0200

    get products from api

--- src/App.js
+++ src/App.js
@@ -4,40 +4,28 @@ import Cart from "./Cart";
 import Filters from "./Filters";
 import Product from "./Product";
 import NavBar from "./NavBar";
+import { useEffect, useState } from "react";
 
 // 1. Impartim in componente
 // 2. Generam produsele cu map
 // 3. Luam produsele din api cu useEffect: https://fakestoreapi.com/products
 
 function App() {
-  const products = [
-    {
-      img: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-      title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-      price: "109.95",
-      rating: "3.9",
-    },
+  const [prod, setProd] = useState([]);
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((response) => response.json())
+      .then((response2) => setProd(response2));
+  }, []);
 
-    {
-      img: "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
-      title: "Mens Casual Premium Slim Fit T-Shirts",
-      price: "22.95",
-      rating: "2.9",
-    },
-
-    {
-      img: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
-      title: "Mens Cotton Jacket",
-      price: "22.95",
-      rating: "2.9",
-    },
-  ].map((product) => {
+  const products = prod.map((product) => {
     return (
       <Product
-        img={product.img}
+        key={product.image}
+        img={product.image}
         title={product.title}
         price={product.price}
-        rating={product.rating}
+        rating={product.rating.rate}
       />
     );
   });
