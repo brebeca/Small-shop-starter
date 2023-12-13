4343b8ab57144960896f9e44e214f0c4814ca6bf
Rebeca <rstingu@gannett.com>
2023-12-13 20:16:09 +0200

    product page loads product from api

--- src/ProductPage.js
+++ src/ProductPage.js
@@ -1,8 +1,16 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const ProductPage = () => {
+  const [product, setProduct] = useState({});
   const params = useParams();
 
-  return <h1>This is product page with title: {params.productID}</h1>;
+  useEffect(() => {
+    fetch(`https://fakestoreapi.com/products/${params.productID}`)
+      .then((response) => response.json())
+      .then((response2) => setProduct(response2));
+  }, [params.productID]);
+
+  return <h1>This is product page with title: {product.title}</h1>;
 };
 export default ProductPage;
