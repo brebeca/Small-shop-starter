2b2c8ced323ac2fc0174af9ba8ce67eb19b86a63
Rebeca <rstingu@gannett.com>
2023-12-13 20:21:37 +0200

    product page html with product details

--- src/ProductPage.js
+++ src/ProductPage.js
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import "./App.css";
 
 const ProductPage = () => {
   const [product, setProduct] = useState({});
@@ -11,6 +12,32 @@ const ProductPage = () => {
       .then((response2) => setProduct(response2));
   }, [params.productID]);
 
-  return <h1>This is product page with title: {product.title}</h1>;
+  return (
+    <div className="prod-container2">
+      <img className="prod-img" src={product.image} alt={"img"} />
+      <p className="prod-title">{product.title}</p>
+
+      <div className="price-container">
+        <p>
+          <small>$</small>
+          <b>{product.price}</b>
+        </p>
+        <p style={{ color: "gray" }}>
+          <small>rating: </small>
+          <b>{product?.rating?.rate}</b>
+        </p>
+      </div>
+      {/* add to redux state */}
+      <button
+        onClick={() => {
+          //   props.setcartItems(props.cartItems + 1);
+          //   setdisableBut(true);
+        }}
+      >
+        Add to cart
+      </button>
+      {/* <button className="remove-btn">Already in cart</button> */}
+    </div>
+  );
 };
 export default ProductPage;
